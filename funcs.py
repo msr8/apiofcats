46dde46d2c0680ad7f9558f1b236a3dd21c16aca
msr8 <live.shashwat@gmail.com>
2022-06-03 00:48:44 +0530

    fixed download issues, and added argument processing

--- funcs.py
+++ funcs.py
@@ -1,6 +1,58 @@
 import random as r
 import os
 
+
+
+def get_file_path(eyebleach_path:str,
+                  allowed_exts:list = ['png','jpg','jpeg','mp4'],
+                  max_size = None) -> str:
+    # Gets all the files 
+    all_files = os.listdir(eyebleach_path)
+    # Shuffles them
+    r.shuffle(all_files)
+    # Loops thro them until it finds a valid file
+    valid_file_found = False
+    for fname in all_files:
+        fp = os.path.join(eyebleach_path, fname)
+        # Checks if its cache
+        if fname.startswith('.'):      continue
+        # Checks if its of a valid extension
+        ext = os.path.splitext(fname)[1][1:]
+        if not ext in allowed_exts:    continue
+        # Checks if it satisfies the size limit
+        size = os.path.getsize(fp)
+        if not max_size == None:
+            if size > max_size:        continue
+        valid_file_found = True
+        break
+
+    if not valid_file_found:
+        return None
+    return {'fp':fp, 'fname':fname, 'ext':ext, 'size':size}
+
+
+
+def process_arguments(request, DEFAULT_EXTS:list):
+    # Gets all the args
+    args              =  request.args
+    ext               =  args.get('ext')
+    max_size          =  args.get('max_size')
+    redirect_arg      =  args.get('redirect')
+    # Processes the arguments
+    ext:list          =  ext.split(',') if ext else DEFAULT_EXTS
+    max_size          =  int(max_size) if max_size else None
+    redirect_arg:bool =  True if redirect_arg=='true' or redirect_arg=='True' else False
+    # Returns the arguments
+    return ext, max_size, redirect_arg
+
+
+
+
+
+
+
+
+'''
 def get_file_path(eyebleach_path:str,
                   allowed_exts:list = ['png','jpg','jpeg','mp4'],
                   max_size = None) -> str:
@@ -23,3 +75,5 @@ def get_file_path(eyebleach_path:str,
         break
 
     return {'fp':fp, 'fname':fname, 'ext':ext, 'size':size}
+'''
+
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask, send_file, send_from_directory, redirect, request
+from flask import Flask, send_file, send_from_directory, redirect, url_for, render_template, request
 from flask_restful import Api, Resource
 from flask_restful import request as request_api
 
-from funcs import get_file_path
+from funcs import get_file_path, process_arguments
 
 
 
@@ -10,7 +10,7 @@ from funcs import get_file_path
 
 
 EYEBLEACH_PATH = '/Users/ravi/Pictures/Eyebleach'
-DOMAIN = '127.0.0.1:5000'
+DOMAIN = 'http://127.0.0.1:5000'
 DEFAULT_EXTS = ['png','jpg','jpeg','mp4']
 app = Flask(__name__)
 api = Api(app)
@@ -30,9 +30,12 @@ api = Api(app)
 
 class Random(Resource):
     def get(self):
-        args = request_api.args
-        print(args)
-        file_dic = get_file_path(EYEBLEACH_PATH)
+        # Gets all the args
+        ext, max_size, _ = process_arguments(request, DEFAULT_EXTS)
+        # Gets the file stuff
+        file_dic = get_file_path(EYEBLEACH_PATH, ext, max_size)
+        # Checks if the file was found
+        if not file_dic:    return {'error':'No file found satisfying the given parameters'}, 301
         fname, ext, size = file_dic['fname'], file_dic['ext'], file_dic['size']
         dic = {
             'filename': fname,
@@ -41,6 +44,7 @@ class Random(Resource):
             'size':     size
         }
         return dic
+api.add_resource(Random, '/api/random')
 
 
 @app.route('/')
@@ -51,19 +55,21 @@ def page_home():
 @app.route('/random')
 def page_random():
     # Gets all the args
-    args =          request.args
-    ext =           args.get('ext')
-    max_size =      args.get('max_size')
-    redirect_arg =  args.get('redirect')
-    # Processes ext
-    ext = ext.split(',') if ext else DEFAULT_EXTS
+    ext, max_size, redirect_arg = process_arguments(request, DEFAULT_EXTS)
+    # Gets the file
+    try:       file_dic = get_file_path(EYEBLEACH_PATH, max_size=max_size, allowed_exts=ext)
+    # If there is an error processing the arguments, shows error
+    except:    file_dic = None
+    # Checks if a file exists with the given parameters
+    if not file_dic:    return send_file('static/error.jpg')
     # Gets the file info
-    file_dic = get_file_path(EYEBLEACH_PATH, max_size=max_size, allowed_exts=ext)
-    fp, fname = file_dic['fp'], file_dic['fname']
+    fp, fname, file_ext = file_dic['fp'], file_dic['fname'], file_dic['ext']
     # If the redirect arg is given, redirects
-    if redirect_arg:    redirect(f'/library/{fname}')
-    # Else, sends the file
-    return send_file(fp)
+    if redirect_arg:    return redirect( url_for('page_library',fname=fname) )
+    # Else, displays the media
+    # return send_file(fp)
+    html_file = 'video.html' if file_ext == 'mp4' else 'image.html'
+    return render_template(html_file, fname=fname, url=f'{DOMAIN}/library/{fname}')
 
 
 @app.route('/library/<fname>')
@@ -71,12 +77,16 @@ def page_library(fname):
     return send_from_directory(EYEBLEACH_PATH, fname)
 
 
+@app.route('/test<arg1>')
+def page_test(arg1):
+    return render_template('photo.html', arg1=arg1)
 
 
 
 
 
-api.add_resource(Random, '/api/random')
+
+
 
 if __name__ == '__main__':
 
@@ -94,12 +104,13 @@ if __name__ == '__main__':
 
 
 '''
--> Fix download
--> Convert webp to png or smth
+-> Add /stats
+-> Fix download (in both, random AND library)
 -> Add min size as a param
--> Add args input (in API)
 -> favicon.ico
--> Change title to filename, or while saving
+-> Gets args in API
+
+http://127.0.0.1:5000/library/ead9beic1jh71.jpeg
 '''
 
 
